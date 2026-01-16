38736bfc579273991565faf9ef5cdd43f89895b5
Samantha Chiachio <157989525+sassational@users.noreply.github.com>
2026-01-16 11:56:18 -0300

    Create Main for Spot Instance Request

---
+++ aws/ec2/aws-eip-association/aws-spot-instance-request/main.tf
@@ -0,0 +1,138 @@
+resource "aws_spot_instance_request" "this" {
+
+  spot_price                      = var.spot_price
+  wait_for_fulfillment            = var.wait_for_fulfillment
+  spot_type                       = var.spot_type
+  launch_group                    = var.launch_group
+  instance_interruption_behavior  = var.instance_interruption_behavior
+  valid_until                     = var.valid_until
+  valid_from                      = var.valid_from
+  tags                            = var.tags
+  
+  ami                                  = var.ami
+  instance_type                        = var.instance_type
+  key_name                             = var.key_name
+  subnet_id                            = var.subnet_id
+  vpc_security_group_ids               = var.vpc_security_group_ids
+  associate_public_ip_address          = var.associate_public_ip_address
+  availability_zone                    = var.availability_zone
+  user_data                            = var.user_data
+  user_data_base64                     = var.user_data_base64
+  iam_instance_profile                 = var.iam_instance_profile
+  monitoring                           = var.monitoring
+  ebs_optimized                        = var.ebs_optimized
+  source_dest_check                    = var.source_dest_check
+  disable_api_termination              = var.disable_api_termination
+  instance_initiated_shutdown_behavior = var.instance_initiated_shutdown_behavior
+  placement_group                      = var.placement_group
+  tenancy                              = var.tenancy
+  host_id                              = var.host_id
+  cpu_core_count                       = var.cpu_core_count
+  cpu_threads_per_core                 = var.cpu_threads_per_core
+  hibernation                          = var.hibernation
+
+  dynamic "root_block_device" {
+    for_each = var.root_block_device
+    content {
+      delete_on_termination = lookup(root_block_device.value, "delete_on_termination", null)
+      encrypted             = lookup(root_block_device.value, "encrypted", null)
+      iops                  = lookup(root_block_device.value, "iops", null)
+      kms_key_id            = lookup(root_block_device.value, "kms_key_id", null)
+      volume_size           = lookup(root_block_device.value, "volume_size", null)
+      volume_type           = lookup(root_block_device.value, "volume_type", null)
+      throughput            = lookup(root_block_device.value, "throughput", null)
+      tags                  = lookup(root_block_device.value, "tags", null)
+    }
+  }
+
+  dynamic "ebs_block_device" {
+    for_each = var.ebs_block_device
+    content {
+      device_name           = ebs_block_device.value.device_name
+      delete_on_termination = lookup(ebs_block_device.value, "delete_on_termination", null)
+      encrypted             = lookup(ebs_block_device.value, "encrypted", null)
+      iops                  = lookup(ebs_block_device.value, "iops", null)
+      kms_key_id            = lookup(ebs_block_device.value, "kms_key_id", null)
+      snapshot_id           = lookup(ebs_block_device.value, "snapshot_id", null)
+      volume_size           = lookup(ebs_block_device.value, "volume_size", null)
+      volume_type           = lookup(ebs_block_device.value, "volume_type", null)
+      throughput            = lookup(ebs_block_device.value, "throughput", null)
+      tags                  = lookup(ebs_block_device.value, "tags", null)
+    }
+  }
+
+  dynamic "ephemeral_block_device" {
+    for_each = var.ephemeral_block_device
+    content {
+      device_name  = ephemeral_block_device.value.device_name
+      virtual_name = ephemeral_block_device.value.virtual_name
+    }
+  }
+
+  dynamic "network_interface" {
+    for_each = var.network_interface
+    content {
+      device_index          = network_interface.value.device_index
+      network_interface_id  = network_interface.value.network_interface_id
+      delete_on_termination = lookup(network_interface.value, "delete_on_termination", null)
+    }
+  }
+
+  dynamic "credit_specification" {
+    for_each = var.credit_specification != null ? [var.credit_specification] : []
+    content {
+      cpu_credits = credit_specification.value.cpu_credits
+    }
+  }
+
+  dynamic "metadata_options" {
+    for_each = var.metadata_options != null ? [var.metadata_options] : []
+    content {
+      http_endpoint               = lookup(metadata_options.value, "http_endpoint", null)
+      http_tokens                 = lookup(metadata_options.value, "http_tokens", null)
+      http_put_response_hop_limit = lookup(metadata_options.value, "http_put_response_hop_limit", null)
+      instance_metadata_tags      = lookup(metadata_options.value, "instance_metadata_tags", null)
+    }
+  }
+
+  dynamic "enclave_options" {
+    for_each = var.enclave_options != null ? [var.enclave_options] : []
+    content {
+      enabled = enclave_options.value.enabled
+    }
+  }
+
+  dynamic "private_dns_name_options" {
+    for_each = var.private_dns_name_options != null ? [var.private_dns_name_options] : []
+    content {
+      enable_resource_name_dns_aaaa_record = lookup(private_dns_name_options.value, "enable_resource_name_dns_aaaa_record", null)
+      enable_resource_name_dns_a_record    = lookup(private_dns_name_options.value, "enable_resource_name_dns_a_record", null)
+      hostname_type                        = lookup(private_dns_name_options.value, "hostname_type", null)
+    }
+  }
+
+  dynamic "capacity_reservation_specification" {
+    for_each = var.capacity_reservation_specification != null ? [var.capacity_reservation_specification] : []
+    content {
+      capacity_reservation_preference = lookup(capacity_reservation_specification.value, "capacity_reservation_preference", null)
+      
+      dynamic "capacity_reservation_target" {
+        for_each = lookup(capacity_reservation_specification.value, "capacity_reservation_target", null) != null ? [capacity_reservation_specification.value.capacity_reservation_target] : []
+        content {
+          capacity_reservation_id = capacity_reservation_target.value.capacity_reservation_id
+        }
+      }
+    }
+  }
+
+  dynamic "launch_template" {
+    for_each = var.launch_template != null ? [var.launch_template] : []
+    content {
+      id      = lookup(launch_template.value, "id", null)
+      name    = lookup(launch_template.value, "name", null)
+      version = lookup(launch_template.value, "version", null)
+    }
+  }
+
+  volume_tags = var.volume_tags
+}
