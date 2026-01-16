e360d686d15d3d010d4086e28f1c67312f947384
Samantha Chiachio <157989525+sassational@users.noreply.github.com>
2026-01-16 11:35:21 -0300

    Add Main to EIP Association

---
+++ aws/ec2/aws-eip-association/main.tf
@@ -0,0 +1,9 @@
+resource "aws_eip_association" "this" {
+  region               = var.region
+  allocation_id        = var.allocation_id
+  allow_reassociation  = var.allow_reassociation
+  instance_id          = var.instance_id
+  network_interface_id = var.network_interface_id
+  private_ip_address   = var.private_ip_address
+  public_ip            = var.public_ip
+}
