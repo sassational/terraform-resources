321ae51f89c9e2a40d92983b4387347239d2a14a
Sassational <samanthachiachio@outlook.com>
2026-01-21 16:51:26 -0300

    Quick FMT Fix

--- aws/vpc/aws-route-table-association/main.tf
+++ aws/vpc/aws-route-table-association/main.tf
@@ -1,5 +1,7 @@
 resource "aws_route_table_association" "this" {
+  
   route_table_id = var.route_table_id
   subnet_id      = var.subnet_id != "" ? var.subnet_id : null
   gateway_id     = var.gateway_id != "" ? var.gateway_id : null
+
 }
