b492e5ce37f303389b0971f384c8cbb1ce8bc5ff
Sassational <samanthachiachio@outlook.com>
2026-01-21 17:13:48 -0300

    Add Some Few New Outputs

--- aws/vpc/aws-subnet/outputs.tf
+++ aws/vpc/aws-subnet/outputs.tf
@@ -1,3 +1,13 @@
+output "subnet_id" {
+  description = "The ID of the subnet"
+  value       = aws_subnet.this.id
+}
+
+output "arn" {
+  description = "The ARN of the subnet"
+  value       = aws_subnet.this.arn
+}
+
 output "vpc_id" {
   description = "The VPC ID"
   value       = aws_subnet.this.vpc_id
@@ -22,3 +32,14 @@ output "availability_zone_id" {
   description = "The availability zone ID of the subnet"
   value       = aws_subnet.this.availability_zone_id
 }
+
+output "owner_id" {
+  description = "The ID of the AWS account that owns the subnet"
+  value       = aws_subnet.this.owner_id
+}
+
+output "tags_all" {
+  description = "A map of tags assigned to the resource, including those inherited from the provider"
+  value       = aws_subnet.this.tags_all
+}
+
